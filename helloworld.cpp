57cfb1313bd6235b9c2cd5761545bd8fa0f79dcb
uestcake <34190329+uestcake@users.noreply.github.com>
2017-12-03 10:59:34 +0800

    Update helloworld.cpp
    add please

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     std::cout << "hello world" << std::endl;
-    std::cout << "Enter two numbers:" << std::endl;
+    std::cout << "please enter two numbers:" << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
     std::cout << "The product is " << v1 * v2 << std::endl;
